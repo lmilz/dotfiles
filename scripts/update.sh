06835da5aa8f735b58b780a94c5be9a2dbb90960
Lars Milz <milzlars88@gmail.com>
2024-12-27 21:03:16 +0100

    change i3 config and add update script

--- scripts/update.sh
+++ scripts/update.sh
@@ -1,9 +1,30 @@
 #!/bin/bash
 
-# update system
+# update system apt
 echo "Update system..."
 sudo apt update && sudo apt upgrade -y
 
-# clean up
+# clean up apt
 echo "Clean up system..."
 sudo apt autoremove -y && sudo apt autoclean
+
+# update configs
+REPO_DIR="$HOME/Projekte/dotfiles/configs" # repo
+TARGER_DIR="$HOME"
+
+sync_files() {
+    echo "update config files..."
+    # zsh
+    echo "zsh..."
+    ZSH_SOURCE="$REPO_DIR/zsh/.zshrc"
+    ZSH_TARGET="$HOME/.zshrc"
+    cp -r "$ZSH_SOURCE" "$ZSH_TARGET"
+
+    # i3
+    echo "i3wm..."
+    WM_SOURCE="$REPO_DIR/i3/config"
+    WM_TARGET="$HOME/.config/i3/config"
+    cp -r "$WM_SOURCE" "$WM_TARGET"
+}
+
+sync_files
