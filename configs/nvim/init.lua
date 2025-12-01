f2e8bbe4aad43c118bdd32ad506d8f6ca4d4ed13
lmilz <milzlars88@gmail.com>
2025-12-01 16:53:14 +0100

    nvim config

---
+++ configs/nvim/init.lua
@@ -0,0 +1,7 @@
+vim.opt.termguicolors = true
+vim.opt.number = true         
+vim.opt.expandtab = true    
+vim.opt.shiftwidth = 2
+vim.opt.tabstop = 2
+vim.cmd("syntax on")
+vim.cmd("colorscheme habamax")
